2c38c9821c92fba54258e840dd4c0c446d271bf1
Fabiana Pereira <fabianaduarte980@gmail.com>
2023-07-17 01:02:12 -0300

    feat: adiciona algoritmo de árvores de decisão

---
+++ src/decisionTrees.py
@@ -0,0 +1,48 @@
+import mahotas
+import os
+import cv2
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+
+def extractFeatures(image):
+  (means, stds) = cv2.meanStdDev(cv2.cvtColor(image, cv2.COLOR_BGR2HSV))
+  colorStats = np.concatenate([means, stds]).flatten()
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  haralick = mahotas.features.haralick(gray).mean(axis = 0)
+  return np.hstack([colorStats, haralick])
+
+def getLabelsAndData(folderPath, label):
+  labels = []
+  data = []
+
+  images = os.listdir(folderPath)
+
+  for image_name in images:
+    image = cv2.imread(folderPath + '/' + image_name)
+    features = extractFeatures(image)
+    labels.append(label)
+    data.append(features)
+  
+  return [labels, data]
+
+def main():
+  (trainHealthyLabels, trainHealthyData) = getLabelsAndData('dataset/Training/healthy_corals', 'healthy')
+  (trainBleachedLabels, trainBleachedData) = getLabelsAndData('dataset/Training/bleached_corals', 'bleached')
+  trainData = np.concatenate(trainHealthyData, trainBleachedData)
+  trainLabels = np.concatenate(trainHealthyLabels, trainBleachedLabels)
+
+  (testHealthyLabels, testHealthyData) = getLabelsAndData('dataset/Testing/healthy_corals', 'healthy')
+  (testBleachedLabels, testBleachedData) = getLabelsAndData('dataset/Testing/bleached_corals', 'bleached')
+  testData = np.concatenate(testHealthyData, testBleachedData)
+  testLabels = np.concatenate(testHealthyLabels, testBleachedLabels)
+
+  model = DecisionTreeClassifier(random_state = 84)
+  print("[INFO] Treinando modelo...")
+  model.fit(trainData, trainLabels)
+
+  print("[INFO] Avaliando modelo...")
+  predictions = model.predict(testData)
+  print(classification_report(testLabels, predictions))
+
+main()
